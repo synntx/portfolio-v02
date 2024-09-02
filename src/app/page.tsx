5fcaa9b141718ac23d8ef8bf36e25af0d380f9a4
Harsh Dana <harshdana1@gmail.com>
2024-09-02 17:07:27 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">
+        Welcome to <span className="text-primary-500">Next.js!</span>
+        <br />
+        <span className="text-primary-100">App tailwind template</span>
+      </h1>
+    </main>
+  );
+}
