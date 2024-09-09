59644a0ccebfdf9b5d9fcd15eb1ba093dc5303f0
Harsh Dana <harshdana1@gmail.com>
2024-09-09 20:53:10 +0530

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,10 @@ export default function Home() {
         <h1 className="font-allura flex gap-3 items-center justify-center text-xl">
           <img
             src="/logo.png"
-            className="w-10 h-10 bg-gradient-to-t from-black/90 to-black/50 dark:bg-none rounded-xl"
+            alt="Harsh's logo"
+            width={40}
+            height={40}
+            className="bg-gradient-to-t from-black/90 to-black/50 dark:bg-none rounded-xl"
           />
           Harsh
         </h1>
@@ -31,18 +34,18 @@ export default function Home() {
         </div>
         <div className="space-y-4">
           <p className="text-lg text-foreground/80 leading-relaxed">
-            Hi! I'm Harsh, a software developer from India with a passion for
-            tech. I specialize in building websites using
+            Hi! I&apos;m Harsh, a software developer from India with a passion
+            for tech. I specialize in building websites using
             <span className="font-semibold">
               {" "}
               JavaScript, TypeScript, React, Next.js, and Node.js
             </span>
-            . I'm always excited to learn and grow in the fast-paced world of
-            software development. Currently learning Rust to get better at
+            . I&apos;m always excited to learn and grow in the fast-paced world
+            of software development. Currently learning Rust to get better at
             low-level programming.
           </p>
           <p className="text-lg text-foreground/80 leading-relaxed">
-            If you'd like to connect, feel free to reach out on{" "}
+            If you&apos;d like to connect, feel free to reach out on{" "}
             <Link
               href="https://twitter.com/harshyadavone"
               className="text-accent hover:text-green-500 tracking-wide transition-colors duration-200 ease-in-out"
@@ -56,7 +59,7 @@ export default function Home() {
             >
               harshdana1@gmail.com
             </Link>
-            . I'd love to hear from you!
+            . I&apos;d love to hear from you!
           </p>
         </div>
       </div>
