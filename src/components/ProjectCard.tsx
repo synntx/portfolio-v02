c1f481caa6cf9721660edff9e09c3662b4b95267
Harsh Dana <harshdana1@gmail.com>
2024-09-18 16:40:41 +0530

    refactor code

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Link from "next/link";
+import { GithubIcon, LinkSquare02Icon } from "./Icon";
+import { useRouter } from "next/navigation";
+
+interface Project {
+  title: string;
+  description: string;
+  technologies: string[];
+  githubLink: string;
+  liveLink: string;
+}
+
+const ProjectCard = ({ project }: { project: Project }) => {
+  const router = useRouter();
+  function handleClick() {
+    router.push(`/projects/${project.title}`);
+  }
+  return (
+    <div
+      onClick={handleClick}
+      className="md:hover:bg-card/20 cursor-pointer py-6 md:py-12 w-full md:p-6 md:hover:scale-[0.97] active:scale-[0.96] duration-200 transition-all border-b border-border border-dashed"
+    >
+      <h3 className="text-2xl font-semibold mb-2">{project.title}</h3>
+      <p className="text-secondary mb-4">{project.description}</p>
+
+      <div className="flex flex-wrap gap-2 mb-4">
+        {project.technologies.map((tech, index) => (
+          <span
+            key={index}
+            className="bg-muted/60 text-foreground/70 px-2 py-1 rounded-lg text-sm"
+          >
+            {tech}
+          </span>
+        ))}
+      </div>
+
+      <div className="grid grid-flow-col items-center gap-4 mt-6 w-full">
+        {project.liveLink && (
+          <Link
+            href={project.liveLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center transition-colors gap-2 text-accent p-2 bg-accent/5 hover:bg-accent/10 rounded-lg"
+          >
+            <LinkSquare02Icon className="w-5 h-5 text-accent" />
+            <span>Live Site</span>
+          </Link>
+        )}
+        {project.githubLink && (
+          <Link
+            href={project.githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center justify-center transition-colors gap-2 text-accent p-2 bg-muted/70 hover:bg-muted  rounded-lg"
+          >
+            <GithubIcon className="w-5 h-5 text-accent" />
+            <span>Source Code</span>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
